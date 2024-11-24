7b7085327a182994aa708ede5bd49a07038b02a1
dwag-cyber <tg1570@gmail.com>
2024-11-24 16:20:23 +0530

    index

--- js/timetable.js
+++ js/timetable.js
@@ -1,73 +1,85 @@
-// Function to save the timetable data
-const saveTimetable = () => {
-    const table = document.getElementById("timetable");
-    const data = [];
-    for (let i = 1; i < table.rows.length; i++) {
-      const row = [];
-      for (let j = 1; j < table.rows[i].cells.length; j++) {
-        row.push(table.rows[i].cells[j].innerText.trim());
+$('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget)
+    var recipient = button.data('whatever') 
+    var modal = $(this)
+    modal.find('.modal-title').text("Add New Class!")
+    modal.find('.modal-body input').val(recipient)
+  })
+  
+  $('#exampleModal1').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget)
+    var recipient = button.data('whatever') 
+    var modal = $(this)
+    modal.find('.modal-title').text("Remove Class!")
+    modal.find('.modal-body input').val(recipient)
+  })
+  
+  var idmanager ={
+      "Monday":0,
+      "Tuesday":0,
+      "Wednesday":0,
+      "Thrusday":0,
+      "Friday":0
+  
+  }
+  document.getElementById("addme").addEventListener("click",function(){
+  
+      var stime=document.getElementById("stime").value;
+      var time=document.getElementById("time").value;
+      var venue=document.getElementById("venue").value;
+      var course=document.getElementById("course").value;
+      var c_color=document.getElementById("c_color").value;
+      var faculty=document.getElementById("faculty").value;
+      var day=document.getElementById("whichday").value;
+  
+      var div = document.createElement("span");
+      div.style.padding="10px"
+  
+      var node = document.createElement("span");               
+      var textnode = document.createTextNode(course);
+      node.appendChild(textnode);  
+      node.style.fontSize="22px"               
+      div.appendChild(node);  
+  
+  
+      var node1 = document.createElement("span");               
+      var textnode1 = document.createTextNode(" "+venue);         
+      node1.appendChild(textnode1);     
+      node1.style.fontSize="19px"            
+      div.appendChild(node1); 
+  
+      let node2 = document.createElement("p");               
+      let textnode2 = document.createTextNode(faculty);        
+      node2.appendChild(textnode2);
+      node2.style.fontSize="15px"                  
+      div.appendChild(node2);   
+      stime=Number(stime)
+      time=Number(time)
+      i=Number(idmanager[day])
+  
+      //Check a element is present on adjacent or same place.
+      if( ((11-stime+time)>0) && ((5>stime && (6-stime-time)>0) || (stime>5))){
+          div.id=day[0]+i
+          div.style.position="absolute";
+          div.className += day
+          div.style.background=c_color; 
+          div.style.marginTop=stime*75+"px"; 
+          div.style.height=time*75+"px";  
+          document.getElementById(day).appendChild(div);
+          document.getElementById("iclose").click();
       }
-      data.push(row);
-    }
-    localStorage.setItem("timetable", JSON.stringify(data));
-    alert("Timetable saved!");
-  };
+      else{
+          alert("Problem Fixing Class To Table!");
+      }	
+  });
   
-  // Function to load the timetable data
-  const loadTimetable = () => {
-    const savedData = JSON.parse(localStorage.getItem("timetable"));
-    if (savedData) {
-      const table = document.getElementById("timetable");
-      for (let i = 0; i < savedData.length; i++) {
-        for (let j = 0; j < savedData[i].length; j++) {
-          table.rows[i + 1].cells[j + 1].innerText = savedData[i][j];
-        }
+  document.getElementById("remove").addEventListener("click",function(){
+      var whichday1=document.getElementById("whichday1").value;
+      var element =document.getElementById("element").value;
+      try{
+      document.querySelector("."+whichday1+":nth-of-type(n+"+element+")").remove();}
+      catch(err){
+      alert("Problem Deleting Component");
       }
-      alert("Timetable loaded!");
-    }
-  };
-  
-  // Function to reset the timetable
-  const resetTimetable = () => {
-    if (confirm("Are you sure you want to reset the timetable?")) {
-      localStorage.removeItem("timetable");
-      location.reload();
-    }
-  };
-  
-  // Function to check if all cells are filled before saving
-  const validateTimetable = () => {
-    const table = document.getElementById("timetable");
-    let isValid = true;
-    for (let i = 1; i < table.rows.length; i++) {
-      for (let j = 1; j < table.rows[i].cells.length; j++) {
-        if (table.rows[i].cells[j].innerText.trim() === "") {
-          isValid = false;
-          break;
-        }
-      }
-    }
-  
-    if (!isValid) {
-      alert("Please fill in all cells before saving.");
-    } else {
-      saveTimetable();
-    }
-  };
-  
-  // Function to handle auto-saving after an edit
-  const autoSave = () => {
-    saveTimetable();
-  };
-  
-  // Attach event listeners to save and reset buttons
-  document.getElementById("saveButton").addEventListener("click", validateTimetable);
-  document.getElementById("resetButton").addEventListener("click", resetTimetable);
-  
-  // Auto save when content of any cell changes
-  const table = document.getElementById("timetable");
-  table.addEventListener("input", autoSave);
-  
-  // Load timetable data on page load
-  window.addEventListener("DOMContentLoaded", loadTimetable);
-  
+      document.getElementById("iiclose").click();
+  })
